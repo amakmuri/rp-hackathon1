7774a2533ec073c4b561d1ef73fdf91d478cd2f6
amakmuri <alishamakmuri@gmail.com>
2016-04-23 16:40:35 -0700

    switch order of text and image

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 
-$('text').text = "What is your Zodiac animal?"
+$('#text').text = "What is your Zodiac animal? Let's find out!"
  
  var month = prompt("Please enter your birth month as a number from 1-12"); 
     
@@ -12,8 +12,10 @@ $('text').text = "What is your Zodiac animal?"
             month = prompt("You entered " + month + ". Please enter your birth month as a number from 1-12");
             }
     }
+    
+    //add text
 
-    //add image based on input
+     //add image based on input
     if (parseInt(month) <= 3) { //3 = "3"
         $('#image').html('<img src="http://www.learnsomething.tips/wp-content/uploads/2013/09/84.jpg">');
     } else if (month <= 6) {
@@ -23,7 +25,6 @@ $('text').text = "What is your Zodiac animal?"
     } else {
         $('#image').html('<img src="http://s3.favim.com/orig/46/adorable-animals-cute-giraffe-Favim.com-424983.jpg">');
     }
-    
-    //add text
+
     $('#text').text("Here's your adorable animal zodiac sign!");
     $('#text').css('color', 'blue');
